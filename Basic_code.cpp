cd16d7a5f5325d86c2f0b2f2c2b7b5a523b3f63c
S R Rayhan <shafiqulrahman66@gmail.com>
2022-01-08 23:02:07 +0600

    Basic code of DFS added

---
+++ Basic_code.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+map<int,vector<int>>vec;
+map<int,bool>vis;
+
+void dfs(int source){
+    vis[source]=true;
+    for(auto i : vec[source]){
+        if(!vis[i]) dfs(i);
+    }
+}
+
+int main(){
+    int node,edge;
+    cin>>node>>edge;//Taking the number of node and number of edge
+
+    for(int i=0;i<edge;i++){
+        int x,y;
+        cin>>x>>y;//taking two nodes
+        vec[x].push_back(y);
+        vec[y].push_back(x);//using adjacency list
+    }
+
+    int source;
+    cin>>source;
+    dfs(source);//DFS call
+
+    for(auto it=vec.begin();it!=vec.end();it++){
+        cout<<"vis["<<it->first<<"] :: "<<vis[it->first]<<endl; // checking that all nodes are visited or not
+    }
+    
+}
